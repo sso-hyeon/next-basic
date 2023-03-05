9bc318e5c880348075b5cdad508c0c99a3cf711f
sso-hyeon <ashash1994@naver.com>
2023-03-05 23:04:12 +0900

    Feat: next로 영화 정보페이지 구현하기

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,43 @@
 import Seo from "../components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {results.map(movie => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3001/api/movies`)).json();
+  return {
+    props: { results }
+  };
 }
